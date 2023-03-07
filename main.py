9f2f263168c68fc6917d40246f5875dded3bbc98
panik <kp1g20@soton.ac.uk>
2023-03-07 03:43:32 +0000

    This fourth prototype now includes a basic DOM rewriter which disables the href attributes on web elements preventing links from being followed

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import selenium.webdriver.support.expected_conditions
 from IPQSAPI import IPQSAPI
 from IPQSScraper import IPQScraper
 from URLVoid import URLVoidScanner
+from DOMRewriter import DOMRewriter
 
 chromeOptions = Options()
 
@@ -18,12 +19,5 @@ driver = webdriver.Chrome(options=chromeOptions, service=driver_service)
 driver.get("https://www.google.co.uk/")
 
 
-
-
-#while True:
-    #driver.switch_to.window(driver.window_handles[len(driver.window_handles) - 1])
-    #links = driver.find_elements(By.XPATH, "//a[@href]")
-    #for link in links:
-        #driver.execute_script("arguments[0].removeAttribute('href')", link)
 
 
